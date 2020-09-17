8c00e935d0ad27835fe2a71b65ce201aa86d166a
Gpkmr <gopi.kumar186@gmail.com>
2020-09-17 16:43:34 +0530

    Add files via upload
    Uploading files to repo

---
+++ server.js
@@ -0,0 +1,175 @@
+var express = require("express")
+var app = express()
+var db = require("./database.js")
+var md5 = require("md5")
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var HTTP_PORT = 8000
+
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+
+app.get("/api/users", (req, res, next) => {
+    var sql = "select * from user"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+});
+
+
+app.get("/api/user/:id", (req, res, next) => {
+    var sql = "select * from user where id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+      });
+});
+
+
+app.post("/api/user/", (req, res, next) => {
+    var errors=[]
+    if (!req.body.password){
+        errors.push("No password specified");
+    }
+    if (!req.body.email){
+        errors.push("No email specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password : md5(req.body.password)
+    }
+    var sql ='INSERT INTO user (name, email, password, last_login_ts) VALUES (?,?,?,"")'
+    var params =[data.name, data.email, data.password]
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "created",
+            "data": data,
+            "id" : this.lastID
+        })
+    });
+})
+
+
+
+app.patch("/api/user/:id", (req, res, next) => {
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password : req.body.password ? md5(req.body.password) : undefined
+    }
+    db.run(
+        `UPDATE user set 
+           name = coalesce(?,name), 
+           email = COALESCE(?,email), 
+           password = coalesce(?,password) 
+           WHERE id = ?`,
+        [data.name, data.email, data.password, req.params.id],
+        (err, result) => {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({
+                message: "updated",
+                data: data
+            })
+    });
+})
+
+
+app.delete("/api/user/:id", (req, res, next) => {
+    db.run(
+        'DELETE FROM user WHERE id = ?',
+        req.params.id,
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({"message":"deleted", rows: this.changes})
+    });
+})
+
+app.post("/auth/", (req, res, next) => {
+    var errors=[]
+    if (!req.body.password){
+        errors.push("No password specified");
+    }
+    if (!req.body.email){
+        errors.push("No email specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+    var data = {
+        email: req.body.email,
+        password : md5(req.body.password)
+    }
+	
+	var chk_sql ='SELECT * FROM user WHERE email=?'
+	var chk_params =[data.email]
+	db.get(chk_sql, chk_params, (err, row) => {
+        if (err) {
+			next(err);
+			return;
+        }
+        if (!row) {
+			res.status(400).json('Invalid email address');
+			return
+		}
+		if (row.email === data.email && row.password === data.password) {
+			var sql ='UPDATE user set last_login_ts=CURRENT_TIMESTAMP WHERE email=?'
+			var params =[data.email]
+			db.run(sql, params, function (err, result) {
+				if (err){
+					res.status(400).json({"error": err.message})
+					return;
+				}
+				res.json({
+					"message": "User credentials verified successfully",
+					"email": data.email,
+				})
+			});
+		}
+		else {
+			res.status(400).json('Invalid password');
+			return
+		}
+    });
+})
+
+
+// Root path
+app.get("/", (req, res, next) => {
+    res.json({"message":"Lets CRUD !"})
+});
